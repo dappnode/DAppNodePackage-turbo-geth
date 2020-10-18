1a88d4329c83792ddda749e5f73e820d4f866339
Eduardo Antuña Díez <eduadiez@gmail.com>
2020-10-18 17:26:22 +0200

    Add restapi

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-tg --lmdb.mapSize=$LMDB_MAPSIZE --private.api.addr=0.0.0.0:9090 &
-exec rpcdaemon --chaindata /root/.local/share/turbogeth/tg/chaindata --http.api=eth,debug,net --http.addr=0.0.0.0 --http.vhosts='*' --ws  
+tg --lmdb.mapSize=$LMDB_MAPSIZE --metrics --metrics.addr="0.0.0.0" --metrics.port="6060" --private.api.addr="0.0.0.0:9090" --pprof --pprof.addr="0.0.0.0" --pprof.port="6061" &
+restapi --http.addr="0.0.0.0:8080" --private.api.addr="localhost:9090" &
+exec rpcdaemon --private.api.addr="localhost:9090" --http.addr="0.0.0.0" --http.vhosts="*" --http.corsdomain="*" --http.api="eth,debug,net"
