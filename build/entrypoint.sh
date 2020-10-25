357eafe391bb704eec87e96172bdc4c2b002011e
Eduardo Antuña Díez <eduadiez@gmail.com>
2020-10-25 18:28:09 +0100

    Bump version

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-tg --lmdb.mapSize=$LMDB_MAPSIZE --metrics --metrics.addr="0.0.0.0" --metrics.port="6060" --private.api.addr="0.0.0.0:9090" --pprof --pprof.addr="0.0.0.0" --pprof.port="6061" &
+tg --lmdb.mapSize=$LMDB_MAPSIZE --metrics --metrics.addr="0.0.0.0" --metrics.port="6060" --private.api.addr="0.0.0.0:9090" ${EXTRA_OPTS} &
 restapi --http.addr="0.0.0.0:8080" --private.api.addr="localhost:9090" &
 exec rpcdaemon --private.api.addr="localhost:9090" --http.addr="0.0.0.0" --http.vhosts="*" --http.corsdomain="*" --http.api="eth,debug,net"
