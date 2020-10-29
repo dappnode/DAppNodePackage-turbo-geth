6f25ae3b290beb151d053039efddd12f8a3a4731
Eduardo Antuña Díez <eduadiez@gmail.com>
2020-10-29 18:51:13 +0100

    Add supervisord

--- build/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-tg --lmdb.mapSize=$LMDB_MAPSIZE --metrics --metrics.addr="0.0.0.0" --metrics.port="6060" --private.api.addr="0.0.0.0:9090" ${EXTRA_OPTS} &
-restapi --http.addr="0.0.0.0:8080" --private.api.addr="localhost:9090" &
-exec rpcdaemon --private.api.addr="localhost:9090" --http.addr="0.0.0.0" --http.vhosts="*" --http.corsdomain="*" --http.api="eth,debug,net"
